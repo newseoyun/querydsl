880173e7e9a97b6b5278b7dc6372c54cbd11d485
macSeoyun <newseoyun@gmail.com>
2021-03-16 23:32:54 +0900

    .

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,83 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static study.querydsl.entity.QMember.member;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        // member1 을 찾아라.
+
+        String qlString = "select m from Member m " +
+                "where m.username = :username";
+
+        Member findMember = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl() { // Querydsl 은 컴파일 시에 문법오류를 발견할 수 있는 큰 장점
+
+        // 엔티티매니저를 쿼리팩토리에 넣어줌. 아래보단 필드에서 주입해주는 것을 권장. 동시성 문제 없음. 멀티스레드에서 분배 해줌.
+        // JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+        // gradle > other > compileQuerydsl 해주면 QMember 가 생성됨 (build.gradle 에 세팅해둔 것)
+        // QMember m = new QMember("m"); // m 은 크게 중요하지 않다. 단지 구분해주는 이름(별칭).
+        // 같은 테이블을 조인해야하는 경우에 위처럼 alias 지정해서 생성해주고, 이것보단 아래처럼
+        // QMember m = QMember.member; 이렇게 쓰는게 한결 깔끔하지만, 또 이걸 스태틱 임포트 해주면 제일 베스트
+
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1")) // prepared statement parameter binding
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+}
--- src/test/java/study/querydsl/entity/MemberTest.java
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -3,15 +3,10 @@ package study.querydsl.entity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
